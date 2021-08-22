26199838dca496d8dc421bb87cdbd4ce1a8e590c
zahlen45 <alecast45@gmail.com>
2021-08-22 12:08:04 +0200

    Optimizacion en el tamaño de los canvas

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,11 @@
 import { Game } from './game';
 import { boardCanvas, heldCanvas, queueCanvas, lockProgressBar } from './constants'
 
-boardCanvas.width = boardCanvas.clientWidth;
-boardCanvas.height = boardCanvas.clientHeight;
+let canvases = [boardCanvas, heldCanvas, queueCanvas, lockProgressBar]
 
-heldCanvas.width = heldCanvas.clientWidth;
-heldCanvas.height = heldCanvas.clientHeight;
-
-queueCanvas.width = queueCanvas.clientWidth;
-queueCanvas.height = queueCanvas.clientHeight;
-
-lockProgressBar.width = lockProgressBar.clientWidth;
-lockProgressBar.height = lockProgressBar.clientHeight;
+canvases.forEach(element => {
+    element.width = element.clientWidth;
+    element.height = element.clientHeight;
+});
 
 var g = new Game()

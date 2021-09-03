2bd82c3ce87a1c8f69299b8a6c4d37e36c838abc
zahlen45 <alecast45@gmail.com>
2021-09-03 21:12:40 +0200

    Creacion de clase y primeras notas

---
+++ src/bot.ts
@@ -0,0 +1,15 @@
+/**
+ * El bot lo unico que necesita son las piezas iniciales y la siguiente en la cola cuando la
+ * haya colocado.
+ * 
+ * El resto de datos se pueden ir "construyendo" por el bot como el tablero o la pieza guardada.
+ */
+export class Bot{
+    
+
+
+    constructor(){
+        
+    }
+
+}

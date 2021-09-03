383711e869daaf18437846142c93d97822972dc0
zahlen45 <alecast45@gmail.com>
2021-09-03 21:15:36 +0200

    Pequeña nota mas

--- src/bot.ts
+++ src/bot.ts
@@ -3,6 +3,8 @@
  * haya colocado.
  * 
  * El resto de datos se pueden ir "construyendo" por el bot como el tablero o la pieza guardada.
+ * 
+ * Es posible que el bot se beneficie de estar en un servidor aparte
  */
 export class Bot{
     
